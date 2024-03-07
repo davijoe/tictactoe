a5b0784586f0262eec652f4f887d09f79f34ad3b
davijoe <61663729+davijoe@users.noreply.github.com>
2024-03-07 23:01:02 +0100

    a new beginning

---
+++ ttt_java/src/Board.java
@@ -0,0 +1,21 @@
+public class Board {
+    private char[][] board;
+    private int boardSize = 3;
+
+    public Board(){
+        for (int row = 0; row < boardSize; row++){
+            for (int col = 0; col < boardSize; col++) {
+                board[row][col] = ' ';
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int row = 0; row < boardSize; row++) {
+            for (int col = 0; col < boardSize; col++) {
+
+            }
+        }
+    }
+
+}
